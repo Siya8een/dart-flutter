dbcef70e40cd93bbfbc297d3cdf276bcd3da2696
Siya Rana <siyarana238@gmail.com>
2022-10-20 01:52:06 +0530

    initial commit

---
+++ bin/main.dart
@@ -0,0 +1,10 @@
+// static type language: if once tye defined can't.f assign dynamically
+// aot and jit compilation
+import 'dart:core';
+import 'dart:io';
+
+void main() {
+  // int-> String
+  String onestring = 1.toString();
+  assert()
+}
